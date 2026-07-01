25447aeb70fe1c4e23c80bc49dc700d9d67e91b6
Nilesh Kamble <kamblenilesh040@gmail.com>
2026-07-01 13:44:19 +0530

    Add custom port and SSL support to PyMySQL connections for deployment compatibility

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 import os
+# pyrefly: ignore [missing-import]
 from dotenv import load_dotenv
 
 load_dotenv()
 
 MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
+MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
 MYSQL_USER = os.getenv("MYSQL_USER", "root")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_DB = os.getenv("MYSQL_DB", "AI_Campus_Placement")
+MYSQL_SSL = os.getenv("MYSQL_SSL", "False").lower() in ("true", "1", "yes")
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
--- database.py
+++ database.py
@@ -1,11 +1,17 @@
 import pymysql
 import config
 
+ssl_config = None
+if config.MYSQL_SSL:
+    ssl_config = {'ssl': {}}
+
 connection = pymysql.connect(
     host=config.MYSQL_HOST,
+    port=config.MYSQL_PORT,
     user=config.MYSQL_USER,
     password=config.MYSQL_PASSWORD,
     database=config.MYSQL_DB,
+    ssl=ssl_config,
     cursorclass=pymysql.cursors.DictCursor
 )
 
